fd1b44e930fd9e6b50f4964fec261e75bdba1a35
Olatunji <sirhollyfat@gmail.com>
2019-11-27 10:07:18 +0100

    Implement function for mexican wave

--- mexican_wave.php
+++ mexican_wave.php
@@ -5,3 +5,30 @@
  * Date: 11/27/2019
  * Time: 9:56 AM
  */
+
+function wave($people){
+
+    //good luck!
+    $output = array();
+
+    $words = str_split(strtolower("$people"));
+    $words_array = strtolower("$people");
+
+    for ($i = 0; $i < count($words); $i++){
+        if($words[$i] == " ")
+            continue;
+        array_push($output, substr_replace($words_array, strtoupper($words[$i]), $i, 1));
+    }
+
+    return $output;
+
+}
+
+
+
+var_dump(wave("hello"));
+var_dump(wave("codeWars"));
+var_dump(wave(""));
+var_dump(wave("two words"));
+var_dump(wave(" gap "));
+//var_dump(wave("codeWars"));
