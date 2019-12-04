85dfdb259e29520a56d684eaeacdd3862d8ea1ff
Olatunji <sirhollyfat@gmail.com>
2019-12-04 14:45:03 +0100

    toWeiedCase: trim string

--- toWeirdCase.php
+++ toWeirdCase.php
@@ -5,3 +5,14 @@
  * Date: 12/4/2019
  * Time: 2:42 PM
  */
+
+/**
+ * @url: https://www.codewars.com/kata/weird-string-case/train/php
+ * */
+
+function toWeirdCase($string) {
+    // TODO
+    $string = trim($string);
+
+
+}
