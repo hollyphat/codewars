6700a0053f3e530de619c2f5240fdcc56a0af072
Olatunji <sirhollyfat@gmail.com>
2019-12-04 11:29:27 +0100

    Implement likes

--- fb_likes.php
+++ fb_likes.php
@@ -5,3 +5,23 @@
  * Date: 12/4/2019
  * Time: 11:24 AM
  */
+
+function likes( $names ) {
+
+    // Your code here...
+    if(!is_array() || count($names) == 0)
+        return "No one likes this";
+
+    if(count($names) < 4){
+        $name_tag = "";
+        for($k =0; $k < count($names); $k++){
+            if($k == count($names) - 1){
+                $name_tag .= $names[$k]. " ";
+            }else{
+                $name_tag .= $name_tag[$k]." and ";
+            }
+        }
+
+        return "$name_tag like this";
+    }
+}
